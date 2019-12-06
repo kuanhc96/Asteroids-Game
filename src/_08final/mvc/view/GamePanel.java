f5fcfc6b6812be893e8069da1acd004752db008d
kuanhc96 <kuanhc96@uchicago.edu>
2019-12-06 00:08:21 -0600

    Completed -- found and fixed a small bug that will some times cause compile error.

--- src/_08final/mvc/view/GamePanel.java
+++ src/_08final/mvc/view/GamePanel.java
@@ -276,7 +276,7 @@ public class GamePanel extends Panel {
 		grpOff.drawString(strDisplay,
 				(Game.DIM.width - fmt.stringWidth(strDisplay)) / 2, Game.DIM.height / 4);
 
-		if (!CommandCenter.getInstance().getGameInitiated()) { // TODO
+		if (!CommandCenter.getInstance().getGameInitiated()) {
 			strDisplay = "use the arrow keys to turn and thrust";
 			grpOff.drawString(strDisplay,
 					(Game.DIM.width - fmt.stringWidth(strDisplay)) / 2, Game.DIM.height / 4
@@ -348,7 +348,7 @@ public class GamePanel extends Panel {
 					(Game.DIM.width - fmt.stringWidth(strDisplay)) / 2, Game.DIM.height / 4
 							+ nFontHeight + 240);
 
-			long timeElapsed = endTime - CommandCenter.getInstance().getGameEpochTime();
+			long timeElapsed = Math.abs(endTime - CommandCenter.getInstance().getGameEpochTime());
 			int minutes = (int)  timeElapsed / 1000 / 60;
 			int seconds = (int) timeElapsed / 1000 % 60;
 			int miliSeconds = (int) timeElapsed % 1000;
